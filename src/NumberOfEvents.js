a461ca77f8fd2085a3fbef8eb0ea1c4749626f26
shplank <lankyjoe@gmail.com>
2021-09-28 15:56:36 -0700

    styling changes

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -15,7 +15,7 @@ class NumberOfEvents extends Component {
     const {numberOfEvents} = this.state;
     return (
       <div className="number-container">
-        <label className="events-number">Number of Events: </label>
+        <label className="label">Number of Events: </label>
         <input 
         type="number" 
         className="number-input"
