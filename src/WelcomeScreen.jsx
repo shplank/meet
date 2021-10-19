64486e6595d72b83d352ee68acf01789b5c2f4be
shplank <lankyjoe@gmail.com>
2021-10-19 08:52:44 -0700

    welcome screen re-styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -6,27 +6,29 @@ function WelcomeScreen(props) {
   return props.showWelcomeScreen ?
   (
   <div className="WelcomeScreen">
-    <h1>Welcome to</h1>
-    <img width="200px" height="90px" src={logo} alt="Logo" className="logo" />
-    <h4>Log in to see upcoming<br/>events around the world<br/>for full-stack developers</h4>
-    <div className="button_cont" align="center">
-      <div class="google-btn">
-        <div class="google-icon-wrapper">
-          <img class="google-icon" 
-          src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
-          alt="Google sign-in" />
+    <div className="WelcomeBox">
+      <h1>Welcome to</h1>
+      <img width="200px" height="90px" src={logo} alt="Logo" className="logo" />
+      <h4>Log in to see upcoming<br/>events around the world<br/>for full-stack developers</h4>
+      <div className="button_cont" align="center">
+        <div class="google-btn">
+          <div class="google-icon-wrapper">
+            <img class="google-icon" 
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" 
+            alt="Google sign-in" />
+          </div>
+          <button onClick={() => { props.getAccessToken() }} 
+            rel="nofollow noopener" 
+            class="btn-text" >
+            <b>Sign in with google</b>                
+          </button>
         </div>
-        <button onClick={() => { props.getAccessToken() }} 
-          rel="nofollow noopener" 
-          class="btn-text" >
-          <b>Sign in with google</b>                
-        </button>
       </div>
+      <a href="https://shplank.github.io/meet/privacy.html" 
+        rel="nofollow noopener" >
+        Privacy policy
+      </a>
     </div>
-    <a href="https://shplank.github.io/meet/privacy.html" 
-      rel="nofollow noopener" >
-      Privacy policy
-    </a>
   </div>
   )
   : null
